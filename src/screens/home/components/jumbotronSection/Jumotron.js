16e6158c343a9c5d33ff37fd429e802e87d692ff
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-06-21 15:35:30 -0400

    starts set to just the jumbotron background

--- src/screens/home/components/jumbotronSection/Jumotron.js
+++ src/screens/home/components/jumbotronSection/Jumotron.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import ParticlesBackground from './ParticlesBackgound';
 import './Jumbotron.css';
+import ParticlesBackground from './ParticlesBackgound';
+
 class Jumbotron extends Component {
   render() {
     return (
-      <div className="jumbotron p-5 rounded-lg text-center">
-        <ParticlesBackground id='particles-bg'/>
+      <div className="jumbotron p-5 rounded-lg text-center position-relative">
+        <ParticlesBackground id="particles" />
         <h1 className="display-4">Justin Chuang</h1>
         <p className="lead">Mechatronic Systems Engineer</p>
       </div>
--- src/screens/home/components/jumbotronSection/ParticlesBackgound.js
+++ src/screens/home/components/jumbotronSection/ParticlesBackgound.js
@@ -1,21 +1,11 @@
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { useEffect, useMemo } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import React, { useEffect, useMemo } from 'react';
+import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
 
-const ParticlesBackground = (props) => {
-  // this should be run only once per application lifetime
+const ParticlesBackground = ({ id }) => {
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     });
   }, []);
 
@@ -25,6 +15,9 @@ const ParticlesBackground = (props) => {
 
   const options = useMemo(
     () => ({
+      fullScreen: {
+        enable: false,
+      },
       background: {
         color: {
           value: "#000000",
@@ -94,7 +87,9 @@ const ParticlesBackground = (props) => {
     []
   );
 
-  return <Particles id={props.id} init={particlesLoaded} options={options} />;
+  return (
+    <Particles id={id} init={particlesLoaded} options={options} className="particles" />
+  );
 };
 
 export default ParticlesBackground;
