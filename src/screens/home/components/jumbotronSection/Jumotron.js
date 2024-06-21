6033d016051d1c72dae8db94b192f06ad7043b05
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-06-21 14:05:02 -0400

    Particles added. need to style still. Versions were a pain but finally work.

--- src/screens/home/components/jumbotronSection/Jumotron.js
+++ src/screens/home/components/jumbotronSection/Jumotron.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ParticlesBackground from './ParticlesBackgound';
 
 class Jumbotron extends Component {
   render() {
     return (
       <div className="jumbotron bg-light p-5 rounded-lg text-center">
+        <ParticlesBackground id="particles" />
         <h1 className="display-4">Justin Chuang</h1>
         <p className="lead">Mechatronic Systems Engineer</p>
       </div>
