58b21ac8dae4102a84acdb0bde23637ceb1c2a13
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-06-24 12:44:48 -0400

    Integral CF font added. Local files not online source.

--- src/screens/home/components/jumbotronSection/Jumotron.js
+++ src/screens/home/components/jumbotronSection/Jumotron.js
@@ -7,7 +7,7 @@ class Jumbotron extends Component {
     return (
       <div className="jumbotron p-5 rounded-lg text-center position-relative">
         <ParticlesBackground id="particles" />
-        <h1 className="display-4">Justin Chuang</h1>
+        <h1 className="display-4">HI I'M JUSTIN CHUANG</h1>
         <p className="lead">Mechatronic Systems Engineer</p>
       </div>
     );
