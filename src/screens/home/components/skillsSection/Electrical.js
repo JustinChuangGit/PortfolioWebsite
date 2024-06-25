67c99833be802c99c55f29a4097fabb21995d118
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-06-25 15:05:30 -0400

    Electrical skill section styled. Added images for electrical softwares. maybe edit the words to make it shorter.

--- src/screens/home/components/skillsSection/Electrical.js
+++ src/screens/home/components/skillsSection/Electrical.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { electrical_background, skillDescriptions_delay, skillDescriptions_duration } from '../constants';
+import './SkillSection.css';
+
+import EagleLogo from '../../../../assets/images/EagleLogo.png';
+import AltiumLogo from '../../../../assets/images/AltiumLogo.png';
+import MicrocapLogo from '../../../../assets/images/microcapLogo.png';
 
 const Electrical = () => {
   return (
@@ -34,17 +39,17 @@ const Electrical = () => {
       }}
       style={{ backgroundColor: electrical_background }}
     >
-        <div className='p-5 col-12'>
+        <div className='p-5 col-12 text-start'>
             <p>From a young age, I was captivated by the intricacies of circuit boards. The way small chips and components could be interconnected to create functional devices seemed almost magical. This fascination grew into a professional passion during my internship, where I was challenged to build devices for conducting various tests. Faced with the need for custom circuit boards to enhance functionality and ensure robustness, I taught myself circuit and PCB design using Eagle software.</p>
             <p>My initial project involved designing and printing my first board, a milestone that marked the beginning of my deep dive into professional PCB development. Since then, I have eagerly embraced opportunities to design and implement professional PCBs in my projects.'m also pretty handy with a soldering iron, making sure all connections are solid and clean. And when things don’t go as planned? I grab an oscilloscope to troubleshoot and fine-tune the system until everything’s running smoothly. </p>
             <p>My expertise extends to integrating microcontrollers like Arduinos, ESP32s, ARM, and Raspberry Pis with my boards, enabling a variety of outputs and applications. This hands-on experience has not only refined my technical abilities but also fueled my continuous pursuit of innovative electrical solutions.</p>
         
             <h4>PCB Software Experience</h4>
-              <ul>
-                <li>Eagle</li>
-                <li>Altium</li>
-                <li>Microcap</li>
-              </ul>
+              <div className='d-flex justify-content-center align-items-center flex-wrap mt-3'>
+                <img src={EagleLogo} alt="Eagle Logo" className="logo-electrical" />
+                <img src={AltiumLogo} alt="Altium Logo" className="logo-electrical" />
+                <img src={MicrocapLogo} alt="Microcap Logo" className="logo-electrical" />
+              </div>
         </div>
     </motion.div>
   );
