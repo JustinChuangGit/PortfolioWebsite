a8411d3e6db0b2e1a7043bcc25f77135902a54d0
JustinChuangGit <80928888+JustinChuangGit@users.noreply.github.com>
2024-07-11 17:56:49 -0400

    Jumbotron optimized for diffent sized screens

--- src/screens/home/components/jumbotronSection/Jumotron.js
+++ src/screens/home/components/jumbotronSection/Jumotron.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import './Jumbotron.css';
-import ParticlesBackground from './ParticlesBackgound';
+import ParticlesBackground from './ParticlesBackground';
 
 class Jumbotron extends Component {
   render() {
     return (
-      <div className="jumbotron p-5 rounded-lg text-center position-relative">
+      <div className="jumbotron py-5 rounded-lg text-center ">
         <ParticlesBackground id="particles" />
         <div className='main-title'>
           <h1>HI IM JUSTIN CHUANG</h1>
         </div>
-
         <div className='sub-title'>
-          <p>I thrive on tackling cutting-edge projects, pushing boundaries, and thinking outside the box to create innovation solutions</p>
+          <p>I thrive on tackling cutting-edge projects, pushing boundaries, and thinking outside the box to create innovative solutions.</p>
         </div>
       </div>
     );
